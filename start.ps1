672df3e908036b2a3cac1b723fdf4ad22662557b
Emilio Parra <emilio.pg95@gmail.com>
2026-05-12 10:19:06 -0600

    Eliminate powershell so the project only uses bash.

--- start.ps1
+++
@@ -1,3 +0,0 @@
-$root = $PSScriptRoot
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "Set-Location '$root\server'; npm run dev" -WindowStyle Normal
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "Set-Location '$root\client'; npm run dev" -WindowStyle Normal
